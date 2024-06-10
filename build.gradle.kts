3a68a8a558ee4093270027d86edd0054d78fea16
jelitatridarm <jelitadarm@gmail.com>
2024-06-10 21:06:29 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,58 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-    }
-}
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
+
+android {
+    namespace = "com.example.projectfinal"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.projectfinal"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation ("com.firebaseui:firebase-ui-database:8.0.1")
+    implementation("com.github.clans:fab:1.6.4")
+    implementation ("com.orhanobut:dialogplus:1.11@aar")
+
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+
+
+
